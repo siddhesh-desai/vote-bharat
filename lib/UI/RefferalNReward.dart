188dcdfa1ac18f9557907c4b1f50c515a9318056
Sanket <sanketdisale871@gmail.com>
2023-04-11 11:27:21 +0530

    RefferalAndReward Screen Added

---
+++ lib/UI/RefferalNReward.dart
@@ -0,0 +1,327 @@
+///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
+
+///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
+
+import 'package:flutter/material.dart';
+
+
+class RefferalNReward extends StatelessWidget {
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+backgroundColor: Color(0xffedecec),
+appBar: 
+AppBar(
+elevation:4,
+centerTitle:false,
+automaticallyImplyLeading: false,
+backgroundColor:Color(0xffffffff),
+shape:RoundedRectangleBorder(
+borderRadius:BorderRadius.zero,
+),
+title:Text(
+"Refferals",
+style:TextStyle(
+fontWeight:FontWeight.w700,
+fontStyle:FontStyle.normal,
+fontSize:18,
+color:Color(0xff000000),
+),
+),
+leading: Icon(
+Icons.menu,
+color:Color(0xff212435),
+size:24,
+),
+actions:[
+Icon(Icons.add_alert,color:Color(0xff212435),size:24),
+Padding(
+padding:EdgeInsets.fromLTRB(0, 0, 12, 0),
+child:Icon(Icons.account_circle_outlined,color:Color(0xff212435),size:24),
+),
+],
+),
+body:
+Stack(
+alignment:Alignment.topLeft,
+children: [
+Container(
+alignment:Alignment.topLeft,
+margin:EdgeInsets.all(0),
+padding:EdgeInsets.fromLTRB(16, 30, 16, 16),
+width:MediaQuery.of(context).size.width,
+height:MediaQuery.of(context).size.height * 0.4,
+decoration: BoxDecoration(
+color:Color(0xff3a57e8),
+shape:BoxShape.rectangle,
+borderRadius:BorderRadius.zero,
+border:Border.all(color:Color(0x4d9e9e9e),width:1),
+),
+child:
+
+Column(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children: [
+Row(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children:[
+
+Container(
+height:70,
+width:70,
+clipBehavior: Clip.antiAlias,
+decoration: BoxDecoration(
+shape: BoxShape.circle,
+),
+child:Image.asset(
+ "images/dollar.png",
+fit:BoxFit.cover),
+),
+Padding(
+padding:EdgeInsets.fromLTRB(16, 0, 0, 0),
+child:Text(
+"Refer Your Friend !",
+textAlign: TextAlign.start,
+overflow:TextOverflow.clip,
+style:TextStyle(
+fontWeight:FontWeight.w400,
+fontStyle:FontStyle.normal,
+fontSize:20,
+color:Color(0xffffffff),
+),
+),
+),
+],),
+Row(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children:[
+
+Text(
+"And Earn Rs.150 Each",
+textAlign: TextAlign.start,
+overflow:TextOverflow.clip,
+style:TextStyle(
+fontWeight:FontWeight.w700,
+fontStyle:FontStyle.normal,
+fontSize:25,
+color:Color(0xffffffff),
+),
+),
+],),
+],),
+),
+Container(
+alignment:Alignment.topLeft,
+margin:EdgeInsets.fromLTRB(16, 180, 16, 16),
+padding:EdgeInsets.all(0),
+width:MediaQuery.of(context).size.width,
+height:MediaQuery.of(context).size.height * 0.38,
+decoration: BoxDecoration(
+color:Color(0xffffffff),
+shape:BoxShape.rectangle,
+borderRadius:BorderRadius.circular(12.0),
+),
+child:
+Padding(
+padding:EdgeInsets.symmetric(vertical: 0,horizontal:16),
+child:
+Column(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children: [
+
+SizedBox(
+height:16,
+width:16,
+),
+Row(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children:[
+
+///***If you have exported images you must have to copy those images in assets/images directory.
+Image(
+image:AssetImage("assets/images/rupee.png"),
+height:45,
+fit:BoxFit.cover,
+),
+
+Column(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children: [
+Padding(
+padding:EdgeInsets.fromLTRB(20, 0, 0, 1),
+child:Align(
+alignment:Alignment(0.0, -0.4),
+child:Text(
+"Total Rewards",
+textAlign: TextAlign.start,
+overflow:TextOverflow.clip,
+style:TextStyle(
+fontWeight:FontWeight.w500,
+fontStyle:FontStyle.normal,
+fontSize:18,
+color:Color(0xffc1bbbb),
+),
+),
+),
+),
+Text(
+"Rs.300",
+textAlign: TextAlign.start,
+overflow:TextOverflow.clip,
+style:TextStyle(
+fontWeight:FontWeight.w700,
+fontStyle:FontStyle.normal,
+fontSize:20,
+color:Color(0xff000000),
+),
+),
+],),
+],),
+Divider(
+color:Color(0xff808080),
+height:16,
+thickness:2,
+indent:0,
+endIndent:0,
+),
+Row(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children:[
+
+
+Column(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children: [
+Padding(
+padding:EdgeInsets.fromLTRB(28, 0, 0, 0),
+child:Text(
+"My Refferal Code",
+textAlign: TextAlign.start,
+overflow:TextOverflow.clip,
+style:TextStyle(
+fontWeight:FontWeight.w500,
+fontStyle:FontStyle.normal,
+fontSize:20,
+color:Color(0xffc1bcbc),
+),
+),
+),
+Padding(
+padding:EdgeInsets.fromLTRB(0, 6, 0, 0),
+child:Text(
+"ABC124",
+textAlign: TextAlign.start,
+overflow:TextOverflow.clip,
+style:TextStyle(
+fontWeight:FontWeight.w700,
+fontStyle:FontStyle.normal,
+fontSize:18,
+color:Color(0xff000000),
+),
+),
+),
+],),
+],),
+Divider(
+color:Color(0xff808080),
+height:16,
+thickness:2,
+indent:2,
+endIndent:0,
+),
+Row(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children:[
+
+
+Column(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children: [
+Padding(
+padding:EdgeInsets.fromLTRB(22.5, 0, 0, 0),
+child:Text(
+"Share Your Code ",
+textAlign: TextAlign.start,
+overflow:TextOverflow.clip,
+style:TextStyle(
+fontWeight:FontWeight.w500,
+fontStyle:FontStyle.normal,
+fontSize:20,
+color:Color(0xffbfbbbb),
+),
+),
+),
+Padding(
+padding:EdgeInsets.fromLTRB(21, 0, 0, 0),
+child:Row(
+mainAxisAlignment:MainAxisAlignment.start,
+crossAxisAlignment:CrossAxisAlignment.center,
+mainAxisSize:MainAxisSize.max,
+children:[
+
+Padding(
+padding:EdgeInsets.all(5),
+child:///***If you have exported images you must have to copy those images in assets/images directory.
+Image(
+image:AssetImage("assets/images/whatsapp.png"),
+width:MediaQuery.of(context).size.width * 0.13,
+fit:BoxFit.cover,
+),
+),
+Padding(
+padding:EdgeInsets.all(5),
+child:///***If you have exported images you must have to copy those images in assets/images directory.
+Image(
+image:AssetImage("assets/images/facebook.png"),
+width:MediaQuery.of(context).size.width * 0.12,
+fit:BoxFit.cover,
+),
+),
+Padding(
+padding:EdgeInsets.all(5),
+child:///***If you have exported images you must have to copy those images in assets/images directory.
+Image(
+image:AssetImage("assets/images/twitter.png"),
+width:MediaQuery.of(context).size.width * 0.12,
+fit:BoxFit.cover,
+),
+),
+Padding(
+padding:EdgeInsets.all(5),
+child:///***If you have exported images you must have to copy those images in assets/images directory.
+Image(
+image:AssetImage("assets/images/instagram.png"),
+width:MediaQuery.of(context).size.width * 0.12,
+fit:BoxFit.cover,
+),
+),
+],),),
+],),
+],),
+],),),
+),
+],),
+)
+;}
+}
